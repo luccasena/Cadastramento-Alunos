38c23643987d46a13a7a59616d47d814ef977767
Lucca de Sena Barbosa <luccasena22@gmail.com>
2025-04-04 18:17:23 -0300

    fix(set_locale): Por algum motivo, já resolvido, essa função não funcionava.

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ void cabecalho(){
     printf("[4] - Remover Aluno por RGM;\n");
     printf("[0] - Sair;\n");
     linhas();
-    printf("Utilize os √≠ndices [x] para poder acessar as funcionalidade!\n");
+    printf("Utilize os Ìndices [x] para poder acessar as funcionalidade!\n");
     linhas();
 
 }
@@ -69,44 +69,44 @@ int isEmpty(Info *Students){
 int inserct_student(Info *Students){
 
     if(isFull(&*Students)){
-        printf("Lista Cheia! N√£o h√° como acessar essa funcionalidade...\n");
+        printf("Lista Cheia! N„o h· como acessar essa funcionalidade...\n");
 
     }else{
         int answer;
 
-        // Atualizando a posi√ß√£o do ultimo cadastro realizado:
+        // Atualizando a posiÁ„o do ultimo cadastro realizado:
         Students->position++;
         int pos = Students->position;
 
         // Inserindo o RGM:
-        printf("Digite o RGM do usu√°rio: ");
+        printf("Digite o RGM do usu·rio: ");
         scanf("%s", Students->College[pos].RGM);
         fflush(stdin);
 
         // Como iremos criar uma lista encadeada para adicionar na lista de disciplinas
-        // Precisamo definir um aonde essa lista ir√° come√ßar (initial) e aonde ir√° terminar (final_list).
+        // Precisamo definir um aonde essa lista ir· comeÁar (initial) e aonde ir· terminar (final_list).
 
         Students->College[pos].initial = NULL;
 
         // Definimos um ponteiro para auxiliar no encadeamento das disciplinas
         diciplines *aux_ptr;
 
-        // Inserindo as diciplinas do usu√°rio
+        // Inserindo as diciplinas do usu·rio
         while(true){
 
-            // Criando um novo local aonde ser√° adicionado uma disciplina e alocando mem√≥ria para suporta-lo.
+            // Criando um novo local aonde ser· adicionado uma disciplina e alocando memÛria para suporta-lo.
             diciplines *new_node = (diciplines *) malloc(sizeof(diciplines));
 
-            // Solicitando o nome da disciplina que o usu√°rio deseja e armazenando na vari√°vel "new_node".
+            // Solicitando o nome da disciplina que o usu·rio deseja e armazenando na vari·vel "new_node".
             printf("Digite o nome de uma disciplina: ");
             fgets(new_node->name_of_dicipline, 255, stdin);
 
-            // Na teoria, se um elemento est√° sendo inserindo em uma lista encadeada ent√£o ele √© o √∫ltimo,
-            // logo ele estar√° apontando para NULL que indica o fim da lista.
+            // Na teoria, se um elemento est· sendo inserindo em uma lista encadeada ent„o ele È o ˙ltimo,
+            // logo ele estar· apontando para NULL que indica o fim da lista.
 
             new_node->next_dicipline = NULL;
 
-            // Se a vari√°vel initial √© igual a NULL, ent√£o o new_node √© o primeiro n√≥ da lista
+            // Se a vari·vel initial È igual a NULL, ent„o o new_node È o primeiro nÛ da lista
 
             if(Students->College[pos].initial == NULL){
                 Students->College[pos].initial= new_node;
@@ -119,10 +119,10 @@ int inserct_student(Info *Students){
 
             linhas();
             while(true){
-                printf("Voc√™ deseja adicionar mais uma disciplina? [1 - Sim][0 - N√£o]: ");
+                printf("VocÍ deseja adicionar mais uma disciplina? [1 - Sim][0 - N„o]: ");
                 if(scanf("%d", &answer) != 1){
                     limpar_tela();
-                    printf("Op√ß√£o Inv√°lida! Tente Novamente...\n");
+                    printf("OpÁ„o Inv·lida! Tente Novamente...\n");
                     linhas();
                     fflush(stdin);
                 }else{
@@ -130,7 +130,7 @@ int inserct_student(Info *Students){
                         break;
                     }else{
                         limpar_tela();
-                        printf("Op√ß√£o Inv√°lida! Tente Novamente...\n");
+                        printf("OpÁ„o Inv·lida! Tente Novamente...\n");
                         linhas();
                     }
 
@@ -145,26 +145,26 @@ int inserct_student(Info *Students){
 
         limpar_tela();
         linhas();
-        printf("Usu√°rio inserido com sucesso!\n");
+        printf("Usu·rio inserido com sucesso!\n");
     }
     return 1;
 }
 
 int remove_student(Info *Students){
     if(isEmpty(&*Students)){
-        printf("Lista Vazia! N√£o h√° como acessar essa funcionalidade...\n");
+        printf("Lista Vazia! N„o h· como acessar essa funcionalidade...\n");
 
     }else{
         char RGM[9];
 
-        printf("Digite o RGM do usu√°rio que ser√° removido: ");
+        printf("Digite o RGM do usu·rio que ser· removido: ");
         scanf("%s", RGM);
 
         int size_of_list = Students->position+1;
         int pos;
         bool found = false;
 
-        // Procura o RGM solicitado para remo√ß√£o e guarda sua posi√ß√£o:
+        // Procura o RGM solicitado para remoÁ„o e guarda sua posiÁ„o:
 
         for(int i = 0; i < size_of_list; i++){
             if(strcmp(RGM, Students->College[i].RGM) == 0){
@@ -177,7 +177,7 @@ int remove_student(Info *Students){
         if(found == false){
             limpar_tela();
             linhas();
-            printf("Usu√°rio N√£o foi encontrado!\n");
+            printf("Usu·rio N„o foi encontrado!\n");
 
         }else{
             for(int i = pos; i < size_of_list; i++){
@@ -185,7 +185,7 @@ int remove_student(Info *Students){
             }
             limpar_tela();
             linhas();
-            printf("Usu√°rio removido com sucesso!\n");
+            printf("Usu·rio removido com sucesso!\n");
             Students->position--;
         }
 
@@ -196,10 +196,10 @@ int remove_student(Info *Students){
 
 void show_students(Info *Students){
     if(isEmpty(&*Students)){
-        printf("Lista Vazia! N√£o h√° como acessar essa funcionalidade...\n");
+        printf("Lista Vazia! N„o h· como acessar essa funcionalidade...\n");
 
     }else{
-        printf("Usu√°rios Cadastrados em nosso Sistema:\n");
+        printf("Usu·rios Cadastrados em nosso Sistema:\n");
         linhas();
         int size_of_list = Students->position + 1;
 
@@ -222,7 +222,7 @@ void show_students(Info *Students){
 
 int search_students(Info *Students){
     if(isEmpty(&*Students)){
-        printf("Lista Vazia! N√£o h√° como acessar essa funcionalidade...\n");
+        printf("Lista Vazia! N„o h· como acessar essa funcionalidade...\n");
 
     }else{
         char RGM[9];
@@ -246,7 +246,7 @@ int search_students(Info *Students){
         }
     }
      if(!found){
-            printf("Aluno com o RGM %s, n√£o foi encontrado!",RGM);
+            printf("Aluno com o RGM %s, n„o foi encontrado!",RGM);
         }
 
     return 1;
@@ -262,7 +262,7 @@ int main(){
     while(true){
 
         cabecalho();
-        printf("Digite uma op√ß√£o acima: ");
+        printf("Digite uma opÁ„o acima: ");
         if(scanf("%d", &choice) != 1){
             limpar_tela();
             linhas();
@@ -308,7 +308,7 @@ int main(){
 
                     break;
                 default:
-                    printf("Op√ß√£o fora do intervalo, tente novamente...\n");
+                    printf("OpÁ„o fora do intervalo, tente novamente...\n");
                     break;
 
             }
