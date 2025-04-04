7a3fc675f20db76c616bb91edb5917c9d9b3c264
Lucca de Sena Barbosa <luccasena22@gmail.com>
2025-04-04 07:08:06 -0300

    fix(remove_student): Corrigido um bug que impedia de acessar uma função.
    Corrigido um erro na função "remove_studentes" que impedia mostrar ao  usuário a lista de estudantes, mesmo que a lista estivesse preenchida.

--- main.c
+++ main.c
@@ -38,7 +38,6 @@ typedef struct diciplines{
 
 typedef struct Information_of_Students{
     char                RGM[9];
-    diciplines          *list_of_dicipline;
     diciplines          *initial;
 
 
@@ -58,63 +57,168 @@ Info create_list(){
     return college;
 }
 
+int isFull(Info *Students){
+    return Students->position+1 == TAM;
+}
+
+int isEmpty(Info *Students){
+    return Students->position == -1;
+
+}
+
 int inserct_student(Info *Students){
-    int answer;
 
-    // Atualizando a posição do ultimo cadastro realizado:
-    Students->position++;
+    if(isFull(&*Students)){
+        printf("Lista Cheia! Não há como acessar essa funcionalidade...\n");
 
-    // Inserindo o RGM:
-    printf("Digite o RGM do usuário: ");
-    scanf("%s", Students->College[Students->position].RGM);
-    fflush(stdin);
+    }else{
+        int answer;
 
-    // Como iremos criar uma lista encadeada para adicionar na lista de disciplinas
-    // Precisamo definir um aonde essa lista irá começar (initial) e aonde irá terminar (final_list).
+        // Atualizando a posição do ultimo cadastro realizado:
+        Students->position++;
+        int pos = Students->position;
 
-    Students->College[Students->position].initial = NULL;
-
-    // Definimos um ponteiro para auxiliar no encadeamento das disciplinas
-    diciplines *aux_ptr;
-
-    // Inserindo as diciplinas do usuário
-    while(true){
-        // Criando um novo local aonde será adicionado uma disciplina e alocando memória para suporta-lo.
-        diciplines *new_node = (diciplines *) malloc(sizeof(diciplines));
-
-        // Solicitando o nome da disciplina que o usuário deseja e armazenando na variável "new_node".
-        printf("Digite o nome de uma disciplina: ");
-        fgets(new_node->name_of_dicipline, 255, stdin);
-
-        // Na teoria, se um elemento está sendo inserindo em uma lista encadeada então ele é o último,
-        // logo ele estará apontando para NULL que indica o fim da lista.
-
-        new_node->next_dicipline = NULL;
-
-        // Se a variável initial é igual a NULL
-
-        if(Students->College->initial == NULL){
-            Students->College[Students->position].initial= new_node;
-            aux_ptr = new_node;
-
-
-        }else {
-            aux_ptr->next_dicipline = new_node;
-            aux_ptr = new_node;
-        }
-
-        printf("Você deseja adicionar mais uma disciplina?: ");
-        scanf("%d", &answer);
+        // Inserindo o RGM:
+        printf("Digite o RGM do usuário: ");
+        scanf("%s", Students->College[pos].RGM);
         fflush(stdin);
-        if (answer == 0){
-            break;
-        }
-    }
-    Students->College[Students->position].list_of_dicipline = Students->College->initial;
 
-    printf("Aluno: %s\nPosição: %d\n\n", Students->College[Students->position].RGM, Students->position);
+        // Como iremos criar uma lista encadeada para adicionar na lista de disciplinas
+        // Precisamo definir um aonde essa lista irá começar (initial) e aonde irá terminar (final_list).
+
+        Students->College[pos].initial = NULL;
+
+        // Definimos um ponteiro para auxiliar no encadeamento das disciplinas
+        diciplines *aux_ptr;
+
+        // Inserindo as diciplinas do usuário
+        while(true){
+
+            // Criando um novo local aonde será adicionado uma disciplina e alocando memória para suporta-lo.
+            diciplines *new_node = (diciplines *) malloc(sizeof(diciplines));
+
+            // Solicitando o nome da disciplina que o usuário deseja e armazenando na variável "new_node".
+            printf("Digite o nome de uma disciplina: ");
+            fgets(new_node->name_of_dicipline, 255, stdin);
+
+            // Na teoria, se um elemento está sendo inserindo em uma lista encadeada então ele é o último,
+            // logo ele estará apontando para NULL que indica o fim da lista.
+
+            new_node->next_dicipline = NULL;
+
+            // Se a variável initial é igual a NULL, então o new_node é o primeiro nó da lista
+
+            if(Students->College[pos].initial == NULL){
+                Students->College[pos].initial= new_node;
+                aux_ptr = new_node;
+
+            }else {
+                aux_ptr->next_dicipline = new_node;
+                aux_ptr = new_node;
+            }
+
+            linhas();
+            while(true){
+                printf("Você deseja adicionar mais uma disciplina? [1 - Sim][0 - Não]: ");
+                if(scanf("%d", &answer) != 1){
+                    limpar_tela();
+                    printf("\nOpção Inválida! Tente Novamente...\n");
+                    linhas();
+                    fflush(stdin);
+                }else{
+                    if(answer == 0 || answer == 1){
+                        break;
+                    }else{
+                        limpar_tela();
+                        printf("Opção Inválida! Tente Novamente...\n");
+                        linhas();
+                    }
+
+                }
+            }
+
+            fflush(stdin);
+            if (answer == 0){
+                break;
+            }
+        }
+
+        limpar_tela();
+        linhas();
+        printf("Usuário inserido com sucesso!\n");
+    }
+    return 1;
+}
+
+int remove_student(Info *Students){
+    if(isEmpty(&*Students)){
+        printf("Lista Vazia! Não há como acessar essa funcionalidade...\n");
+
+    }else{
+        char RGM[9];
+
+        printf("Digite o RGM do usuário que será removido: ");
+        scanf("%s", RGM);
+
+        int size_of_list = Students->position+1;
+        int pos;
+        bool found = false;
+
+        // Procura o RGM solicitado para remoção e guarda sua posição:
+
+        for(int i = 0; i < size_of_list; i++){
+            if(strcmp(RGM, Students->College[i].RGM) == 0){
+                pos = i;
+                found = true;
+                break;
+            }
+        }
+
+        if(found == false){
+            limpar_tela();
+            linhas();
+            printf("Usuário Não foi encontrado!\n");
+
+        }else{
+            for(int i = pos; i < size_of_list; i++){
+                Students->College[i] = Students->College[i + 1];
+            }
+            limpar_tela();
+            linhas();
+            printf("Usuário removido com sucesso!\n");
+            Students->position--;
+        }
+
+    }
 
     return 1;
+}
+
+void show_students(Info *Students){
+    if(isEmpty(&*Students)){
+        printf("Lista Vazia! Não há como acessar essa funcionalidade...\n");
+
+    }else{
+        printf("Usuários Cadastrados em nosso Sistema:\n");
+        linhas();
+        int size_of_list = Students->position + 1;
+
+        for(int i = 0; i < size_of_list; i++){
+            printf("%d. RGM: %s \n", i+1,Students->College[i].RGM);
+            diciplines *current = Students->College[i].initial;
+            while(current != NULL){
+                printf("  Disciplina: %s", current->name_of_dicipline);
+                current = current->next_dicipline;
+            }
+            if(i < size_of_list-1){
+                linhas();
+            }
+
+        }
+        system("pause");
+        limpar_tela();
+    }
+
 }
 
 int main(){
@@ -138,27 +242,35 @@ int main(){
             switch(choice){
                 case 1:
                     printf("[1] - Realizar Cadastro;\n");
+                    linhas();
 
                     inserct_student(&Students);
-                    // limpar_tela();
-                    linhas();
-                    printf("Usuário inserido com sucesso!\n");
-
 
                     break;
                 case 2:
                     printf("[2] - Mostrar Alunos;\n");
+                    linhas();
+
+                    show_students(&Students);
 
                     break;
                 case 3:
                     printf("[3] - Procurar Aluno por RGM;\n");
+                    linhas();
 
                     break;
                 case 4:
                     printf("[4] - Remover Aluno por RGM;\n");
+                    linhas();
+
+                    remove_student(&Students);
 
                     break;
                 case 0:
+                    printf("[0] - Sair;\n");
+                    linhas();
+
+                    printf("Obrigado por Utilizar nosso programa!\n");
 
                     break;
                 default:
@@ -167,6 +279,9 @@ int main(){
 
             }
 
+        }
+        if(choice == 0){
+            break;
         }
 
     }
